368fea44ee252614b02566731ed7e1f2edccb1f0
Иван Смолин <ivan.smolin.78@mail.ru>
2022-12-08 14:57:06 +0300

    patch 1.1

--- ServerConnectC/ServerConnectC.c
+++ ServerConnectC/ServerConnectC.c
@@ -7,55 +7,43 @@ int main()
 {
 	system("chcp 1251");
 	HANDLE hNamedPipe;
-	DWORD size_buffer = SIZE_BUFFER;
+	
 	LPSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";
-	LPWSTR buffer = (CHAR*)calloc(size_buffer, sizeof(CHAR));
-	char message[SIZE_BUFFER];
-	BOOL Connected;
-	DWORD actual_readen;
-	BOOL SuccessRead;
+	
 
 	while (TRUE)
 	{
 		hNamedPipe = CreateNamedPipe(//создание канала
 			lpszPipeName,//Имя канала
-			PIPE_ACCESS_DUPLEX,//режим доступа к каналу(односторонний/двусторонний)
-			PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,//режим работы канала: передавать сообщения/читать сообщения/ждать
-			PIPE_UNLIMITED_INSTANCES,//количество водящих к каналу, в данном случае неограничено
-			SIZE_BUFFER,//объем буфера на чтение (байт)
-			SIZE_BUFFER,//объем буфера на запись (байт)
-			INFINITE,// максимальное время ожидания сообщения
-			NULL);//указатель на структуру безопасности
-		Connected = ConnectNamedPipe(hNamedPipe, NULL);
-		if (Connected)
+			GENERIC_READ | GENERIC_WRITE,
+			0, NULL, OPEN_EXISTING, 0, NULL
+		);
+		
+		DWORD dwMode = PIPE_READMODE_MESSAGE;
+
+		BOOL isSuccess = SetNamedPipeHandleState(
+			hNamedPipe,
+			&dwMode,
+			NULL,
+			NULL);
+		if (!isSuccess)
 		{
-			SuccessRead = ReadFile(hNamedPipe, buffer, size_buffer, &actual_readen, NULL);
+			printf("Сервер не отвечает\n");
 		}
-	}
-	DWORD dwMode = PIPE_READMODE_MESSAGE;
+		else
+		{
+			printf("Соединение установлено\n");
+			DWORD size_buffer = SIZE_BUFFER;
+			DWORD actual_written;
 
-	BOOL isSuccess = SetNamedPipeHandleState(
-		hNamedPipe,
-		&dwMode,
-		NULL,
-		NULL);
-	if (!isSuccess)
-	{
-		printf("Сервер не отвечает\n");
+			char message[SIZE_BUFFER];
+			printf("Введите сообщение для сервера\n");
+			gets(message);
+			LPWSTR buffer = &message;
+			WriteFile(hNamedPipe, buffer, size_buffer,
+				&actual_written, NULL);
+		}
+		Sleep(1000);
+		CloseHandle(hNamedPipe);
 	}
-	else
-	{
-		printf("соединение установлено\n");
-		
-		
-
-		
-		printf("введите сообщение для сервера:\n");
-		gets(message);
-		LPWSTR buffer = &message;
-		WriteFile(hNamedPipe, buffer, size_buffer,
-			&actual_written, NULL);
-	}
-	Sleep(1000);
-	CloseHandle(hNamedPipe);
 }
