7b64d880a601c7473af4d66962ccf079ad4a24cf
Иван Смолин <ivan.smolin.78@mail.ru>
2022-12-09 14:53:00 +0300

    patch 1.3

---
+++ Bot/Bot.c
@@ -0,0 +1,68 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <Windows.h>
+#include <math.h>
+#define SIZE_BUFFER 140
+
+
+double quadro(char*  ) {
+
+
+
+
+}
+
+int main()
+{
+	system("chcp 1251");
+
+	HANDLE hNamedPipe;
+	LPSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";
+	DWORD size_buffer = 100;
+	LPWSTR buffer = (CHAR*)calloc(size_buffer, sizeof(CHAR));
+	BOOL Flag = TRUE;
+	while (TRUE)
+	{
+
+		hNamedPipe = CreateNamedPipe(//создание канала
+			lpszPipeName,//Имя канала
+			PIPE_ACCESS_DUPLEX,//режим доступа к каналу(односторонний/двусторонний)
+			PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,//режим работы канала: передавать сообщения/читать сообщения/ждать
+			PIPE_UNLIMITED_INSTANCES,//количество водящих к каналу, в данном случае неограничено
+			512,//объем буфера на чтение (байт)
+			512,//объем буфера на запись (байт)
+			INFINITE,// максимальное время ожидания сообщения
+			NULL);//указатель на структуру безопасности
+
+
+		BOOL Connected = ConnectNamedPipe(hNamedPipe, NULL);
+		if (Connected)
+		{
+			printf("\nКлиент успешно подключился\n");
+
+
+
+			DWORD actual_readen;
+			BOOL SuccessRead = ReadFile(hNamedPipe, buffer, size_buffer, &actual_readen, NULL);
+
+			if (!SuccessRead)
+			{
+				printf("\nКлиент пишет: \n");
+				printf(buffer);
+				printf("\n");
+
+				char message[100];
+				printf("\nВведите сообщение для клиента\n");
+				gets(message);
+				buffer = &message;
+				WriteFile(hNamedPipe, buffer, size_buffer, &actual_readen, NULL);
+			}
+		}
+		else
+		{
+			printf("\nКлиент отключился от сервера\n");
+		}
+		printf(buffer);
+		CloseHandle(hNamedPipe);
+	}
+}
--- ServerConnectC/ServerConnectC.c
+++ ServerConnectC/ServerConnectC.c
@@ -7,13 +7,17 @@ int main()
 {
 	system("chcp 1251");
 	HANDLE hNamedPipe;
-	
+	DWORD chit_buff = 100;
+	char message[100];
+	LPWSTR buffer = (CHAR*)calloc(chit_buff,sizeof(CHAR));
 	LPSTR lpszPipeName = L"\\\\.\\pipe\\MyPipe";
-	
+	BOOL Flag_answer = TRUE;
+	DWORD size_buffer = SIZE_BUFFER;
+	DWORD actual_written;
 
 	while (TRUE)
 	{
-		hNamedPipe = CreateNamedPipe(//создание канала
+		hNamedPipe = CreateFile(//создание канала
 			lpszPipeName,//Имя канала
 			GENERIC_READ | GENERIC_WRITE,
 			0, NULL, OPEN_EXISTING, 0, NULL
@@ -29,19 +33,36 @@ int main()
 		if (!isSuccess)
 		{
 			printf("Сервер не отвечает\n");
+			Flag_answer = TRUE;
 		}
 		else
 		{
-			printf("Соединение установлено\n");
-			DWORD size_buffer = SIZE_BUFFER;
-			DWORD actual_written;
+			if (Flag_answer) 
+			{
+				
+				printf("Введите сообщение для сервера\n");
+				gets(message);
+				
+				WriteFile(hNamedPipe, buffer, size_buffer,
+					&actual_written, NULL);
+				Flag_answer = FALSE;
 
-			char message[SIZE_BUFFER];
-			printf("Введите сообщение для сервера\n");
-			gets(message);
-			LPWSTR buffer = &message;
-			WriteFile(hNamedPipe, buffer, size_buffer,
-				&actual_written, NULL);
+			}
+			if (isSuccess)
+			{
+				printf("Сервер пишет\n");
+				printf(buffer);
+				printf("\n");
+				Flag_answer = TRUE;
+				if (buffer)
+					printf("Пусто\n");
+			
+
+				
+			}
+			printf("Соединение установлено\n");
+			
+			
 		}
 		Sleep(1000);
 		CloseHandle(hNamedPipe);
